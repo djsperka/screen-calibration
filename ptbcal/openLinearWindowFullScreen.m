3b0711552673480a5b5a32db58cc86863df61570
Daniel Sperka <djsperka@ucdavis.edu>
2025-04-03 15:31:12 -0700

    Open window with bkgd value, automatically adjusted for 0-1 or 0-255. Validate process incorporates plots, randomized color presentations.

--- ptbcal/openLinearWindowFullScreen.m
+++ ptbcal/openLinearWindowFullScreen.m
@@ -1,4 +1,4 @@
-function [windowIndex, windowRect] = openLinearWindowFullScreen(screen, calfile, b255)
+function [windowIndex, windowRect] = openLinearWindowFullScreen(screen, calfile, b255, bkgd)
 %openLinearWindowFullScreen Opens a full screen window with an inverse
 %gamma table taken from the calfile. If all goes well, this screen will
 %provide linear outputs. If b255 is true, use r,g,b color values in the
@@ -8,7 +8,34 @@ function [windowIndex, windowRect] = openLinearWindowFullScreen(screen, calfile,
     arguments
         screen (1,1) {mustBeNumeric},
         calfile char {mustBeFile},
-        b255 {mustBeNumericOrLogical} = false   
+        b255 {mustBeNumericOrLogical} = false,
+        bkgd {mustBeColor} = 0.5
+    end
+
+
+    % figure out how to deal with the bkgd we were given.
+    isSmall = all(bkgd<=1);
+
+    % open window
+    PsychDefaultSetup(2);
+    if ~b255
+        if isSmall
+            useBkgd = bkgd;
+        else
+            useBkgd = bkgd/255;
+        end
+        [windowIndex, windowRect] = PsychImaging('OpenWindow', screen, useBkgd);
+        fprintf('\nBackground color is %s\n', formatColor(useBkgd));
+        fprintf('Use floating point range 0.0-1.0 for colors in this window.\n');
+    else
+        if isSmall
+            useBkgd = bkgd*255;
+        else
+            useBkgd = bkgd;
+        end
+        [windowIndex, windowRect] = Screen('OpenWindow', screen, useBkgd);
+        fprintf('\nBackground color is %s\n', formatColor(useBkgd));
+        fprintf('Use uint8 range 0-255 for colors in this window.\n');
     end
 
     % cal file
@@ -17,19 +44,24 @@ function [windowIndex, windowRect] = openLinearWindowFullScreen(screen, calfile,
     % inverse gamma
     igamma = InvertGammaTable(cal.gammaInput, cal.gammaTable, 1024);
 
-    % open window
-    PsychDefaultSetup(2);
-    if ~b255
-        [windowIndex, windowRect] = PsychImaging('OpenWindow', screen, .5);
-        fprintf('Use floating point range 0.0-1.0 for colors in this window.\n');
-    else
-        [windowIndex, windowRect] = Screen('OpenWindow', screen, 127);
-        fprintf('Use uint8 range 0-255 for colors in this window.\n');
-    end
-
     % now load inverse gamma table
     Screen('LoadNormalizedGammaTable', windowIndex, igamma, 0);
     Screen('Flip', windowIndex);
 
+    fprintf('Inverse gamma loaded and in use for this window.\n');
+
 end
 
+function mustBeColor(c)
+    assert(isnumeric(c) && isvector(c) && ismember(length(c), [1,3]));
+end
+
+% assume scalar or 3-element vector
+function [str] = formatColor(vecOrScalar)
+    if length(vecOrScalar)==1
+        str = sprintf('[%f]', vecOrScalar);
+    else
+        str = sprintf('[%f,%f,%f]', vecOrScalar(1), vecOrScalar(2), vecOrScalar(3));
+    end
+end
+            
--- ptbcal/validatePTBGamma.m
+++ ptbcal/validatePTBGamma.m
@@ -1,34 +1,28 @@
-function [lumR,lumG,lumB,lumW] = validatePTBGamma(windowIndex, calfile)
+function [lumR,lumG,lumB,lumW] = validatePTBGamma(screen, calfile, npts)
 %validatePTBGamma Validates linearity of current monitor using fixstim
 %(connect via tcp) and spectrometer. 
 %   Spectrometer assumed to be connected (you call old650init prior)
 
-if ~old650isReady()
-    error('old650 not initialized. Call old650init(comport) first.');
-end
+    arguments
+        screen {mustBeNumeric},
+        calfile char {mustBeFile},
+        npts {mustBePositive} = 11
+    end
 
-% % Open window on screen 
-% PsychDefaultSetup(2);
-% w = PsychImaging('OpenWindow', screen, [.5, .5, .5]);
 
-% Load cal file and get gamma table
-% cal = myLoadCalFile(calfile);
-
-% Default spectra returned from pr650.
-S0 = [380, 4, 101];
-
-% T will be needed to compute luminance.
-load T_cones_ss2
-load T_ss2000_Y2
-T = SplineCmf(S_ss2000_Y2, 683*T_ss2000_Y2, S0);
-
+    if ~old650isReady()
+        error('old650 not initialized. Call old650init(comport) first.');
+    end
+    
+    % open window
+    [windowIndex, ~] = openLinearWindowFullScreen(screen, calfile, false, .5);
 
 % values to test
-values = (0:.1:1)';
-r=zeros(length(values),3);
-g=zeros(length(values),3);
-b=zeros(length(values),3);
-w=zeros(length(values),3);
+values = linspace(0, 1, npts)';
+r=zeros(npts,3);
+g=zeros(npts,3);
+b=zeros(npts,3);
+w=zeros(npts,3);
 r(:,1) = values;
 g(:,2) = values;
 b(:,3) = values;
@@ -36,24 +30,48 @@ w(:,1) = values;
 w(:,2) = values;
 w(:,3) = values;
 
+% make a stack of r;g;b;w, 4*npts X 3
+rgbwStack=vertcat(r,g,b,w);
 
-fprintf(1,'Pausing for 10 sec...');
-pause(10);
+% randomize the order of the colors presented
+iorder = randperm(size(rgbwStack, 1));
 
-lumR = measure_lum(windowIndex, r, S0, T);
-lumG = measure_lum(windowIndex, g, S0, T);
-lumB = measure_lum(windowIndex, b, S0, T);
-lumW = measure_lum(windowIndex, w, S0, T);
+fprintf(1,'Pausing for 5 sec...');
+pause(5);
 
-% plot
+% spectral params for PR650 and T for luminance calculation.
+% S0 is determined by the spectrometer - it is the default spectrum param
+% [381,4,101] or something like that.
+[S0, T] = getLumTS();
+
+% measure the luminances
+luminances = measure_lum(windowIndex, rgbwStack(iorder,:), S0, T);
+
+% rearrange for plotting
+lumtmp(iorder(:)) = luminances(:);
+lumRGBW = reshape(lumtmp, npts, 4);
+lumR = lumRGBW(:,1);
+lumG = lumRGBW(:,2);
+lumB = lumRGBW(:,3);
+lumW = lumRGBW(:,4);
+
+% linear fit to each line
+cR = polyfit(values, lumR, 1);
+cG = polyfit(values, lumG, 1);
+cB = polyfit(values, lumB, 1);
+cW = polyfit(values, lumW, 1);
+
+% plot nice
 figure;
-plot(values, lumR, 'r');
-hold on;
-plot(values, lumG, 'g');
-plot(values, lumB, 'b');
-plot(values, lumW, 'k');
-hold off;
+plot(values, lumR, 'r+', values, lumG, 'g+', values, lumB, 'b+', values, lumW, 'k+');
+hold on
+plot(values, polyval(cR,values), 'r');
+plot(values, polyval(cG,values), 'g');
+plot(values, polyval(cB,values), 'b');
+plot(values, polyval(cW,values), 'k');
 
+title('Inverse Gamma Validation');
+ylabel('Measured luminance (Cd/m**2)');
 end
 
 
@@ -64,9 +82,9 @@ function [lum] = measure_lum(w, colors, S, T)
 
         Screen('FillRect', w, colors(i,:));
         Screen('Flip', w);
-        [spd, qual] = old650measspd(S,'on');
+        [spd, qual] = old650measspd(S,'off');
         lum(i) = T * spd;
-        fprintf(1, 'color (%f,%f,%f), qual %d lum %f\n', colors(i,1), colors(i,2), colors(i,3), qual, lum(i));
+        fprintf(1, '%d/%d color (%f,%f,%f), qual %d lum %f\n', i, size(colors, 1), colors(i,1), colors(i,2), colors(i,3), qual, lum(i));
     
     end
 
